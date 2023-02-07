940128e58ab28e90c2f9cb8823eafd6078b0cb22
Lucasfth <luha@itu.dk>
2023-02-07 11:12:03 +0100

    live up to exercise 2

--- app/src/main/java/dk/itu/moapd/scootersharing/mroa/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/mroa/MainActivity.kt
@@ -1,15 +1,49 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Lucas Hanson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package dk.itu.moapd.scootersharing.mroa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import dk.itu.moapd.scootersharing.mroa.databinding.ActivityMainBinding
+
+/**
+ * MainActivity is a class that starts the functionality of the Scooter Sharing app.
+ *
+ * @author Mads Roager (mroa) and Lucas Hanson (luha)
+ */
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
     companion object {
         private val TAG = MainActivity::class.qualifiedName
     }
@@ -19,33 +53,61 @@ class MainActivity : AppCompatActivity() {
     private lateinit var startRide: Button
 
     private val scooter: Scooter = Scooter("", "")
+
+    /**
+     * Called upon app start-up.
+     * Instantiates needed classes.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        scooterName = findViewById(R.id.edit_text_name)
-        scooterLocation = findViewById(R.id.edit_text_location)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        startRide = findViewById(R.id.click_button_start_ride)
+        scooterName = mainBinding.editTextName
+        scooterLocation = mainBinding.editTextLocation
+        startRide = mainBinding.clickButtonStartRide
+
         startRide.setOnClickListener {
             if (scooterName.text.isNotEmpty() &&
                     scooterLocation.text.isNotEmpty()) {
                 val name = scooterName.text.toString().trim()
                 val location = scooterLocation.text.toString().trim()
 
-                scooter.setName(name)
-                scooter.setLocation(location)
+                scooter.name = name
+                scooter.location = location
+
+                hideKeyboard()
 
                 scooterName.setText("")
                 scooterLocation.setText("")
 
+                printMessage()
                 showMessage()
             }
         }
+
+        setContentView(mainBinding.root)
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager =
+            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        val view: View? = this.currentFocus
+
+        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 
     private fun showMessage() {
+        Snackbar.make(mainBinding.root, scooter.toString(), Snackbar.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Prints Scooter that has been inputted into the app
+     */
+    private fun printMessage() {
         Log.d(TAG, scooter.toString())
     }
 }
