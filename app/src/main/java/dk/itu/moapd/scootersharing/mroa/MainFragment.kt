146d46b08ac849e9ea0b3d506c7bb7169484fd70
Lucasfth <luha@itu.dk>
2023-03-07 13:16:12 +0100

    not import list_rides properly

--- app/src/main/java/dk/itu/moapd/scootersharing/mroa/MainFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/mroa/MainFragment.kt
@@ -10,6 +10,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.WindowCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dk.itu.moapd.scootersharing.mroa.databinding.ListRidesBinding
 import dk.itu.moapd.scootersharing.mroa.databinding.ActivityMainBinding
 import dk.itu.moapd.scootersharing.mroa.databinding.FragmentMainBinding
 
@@ -36,6 +39,7 @@ class MainFragment : Fragment() {
     companion object {
         private val TAG = MainFragment::class.qualifiedName
         lateinit var ridesDB : RidesDB
+        private lateinit var adapter: CustomAdapter
     }
 
     /**
@@ -45,6 +49,7 @@ class MainFragment : Fragment() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ridesDB = activity?.let { RidesDB.get(it) }!!
     }
 
     /**
@@ -72,10 +77,14 @@ class MainFragment : Fragment() {
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapter = CustomAdapter(ridesDB.getRidesList())
         val navHostFragment = activity?.supportFragmentManager
             ?.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         val navController = navHostFragment.navController
         with (binding) {
+            mainList.layoutManager = LinearLayoutManager(activity)
+            mainList.adapter = adapter
+
             clickButtonStartRide.setOnClickListener {
                 navController.navigate(R.id.show_start_ride)
             }
@@ -99,4 +108,39 @@ class MainFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private inner class CustomAdapter(
+        private val data: List<Scooter>) :
+        RecyclerView.Adapter<ViewHolder>() {
+
+        //ArrayAdapter<Scooter>(context, R.layout.list_rides, data) {
+        override fun onCreateViewHolder(parent: ViewGroup,
+                                        viewType: Int): ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ListRidesBinding.inflate(
+                inflater, parent, false)
+            return ViewHolder(binding)
+        }
+        override fun getItemCount() = data.size
+        override fun onBindViewHolder(holder: ViewHolder,
+                                      position: Int) {
+            val dummy = data[position]
+            holder.bind(dummy)
+        }
+    }
+
+    inner class ViewHolder(private val binding: ListRidesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(scooter: Scooter) {
+            with (binding) {
+                scooterName.text = root.context.getString(
+                    R.string.scooter_name)
+                scooterLocation.text = root.context.getString(
+                    R.string.scooter_location)
+                scooterTimestamp.text = root.context.getString(
+                    R.string.scooter_timestamp)
+            }
+        }
+    }
 }
+
