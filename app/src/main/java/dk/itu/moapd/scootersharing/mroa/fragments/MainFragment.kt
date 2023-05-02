e8cec2fccafb3f9e63a0e91ae903760e0033df3e
Lucasfth <luha@itu.dk>
2023-05-02 21:28:40 +0200

    Add init rent scooter page
    The rent scooter page is called fragment_selected_scooter.
    It is not fully added into the nav_graph yet since I have
    trouble inserting the links.
    Have also addeded a listener to listen when a google map marker
    is clicked. Right now it only gets the name from the marker and
    nothing else.

--- app/src/main/java/dk/itu/moapd/scootersharing/mroa/fragments/MainFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/mroa/fragments/MainFragment.kt
@@ -10,14 +10,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.LocationSource
-import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -25,7 +24,6 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.getValue
 import dk.itu.moapd.scootersharing.mroa.PrefSingleton
 import dk.itu.moapd.scootersharing.mroa.R
 import dk.itu.moapd.scootersharing.mroa.activities.MainActivity
@@ -198,6 +196,12 @@ class MainFragment : Fragment(), OnMapReadyCallback {
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
+
+        map.setOnMarkerClickListener { marker ->
+            val markerName = marker.title
+            println("$markerName") // This print the marker that has been clicked
+            false
+        }
     }
 
     /**
